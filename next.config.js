931452eb677a7c7e31c6a159a197a6f45bae012e
danielbarion <danielbarionn@gmail.com>
2020-01-27 10:43:45 -0300

    Add package.json missed in the last commit and next config

---
+++ next.config.js
@@ -0,0 +1,23 @@
+const withCSS = require('@zeit/next-css')
+const dotEnv = require('dotenv-flow')
+
+dotEnv.config()
+
+module.exports = withCSS({
+  cssModules: true,
+  poweredByHeader: false,
+  // env: {},
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000,
+          name: '[name].[ext]',
+        },
+      },
+    })
+    return config
+  },
+})
