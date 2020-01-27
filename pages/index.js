2e75d8abb1d2998bbd770faaf9d0dcbb812df7a3
danielbarion <danielbarionn@gmail.com>
2020-01-27 10:57:46 -0300

    Add home page with lorem ipsum

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import HomePage from '../src/pages/Home'
+
+const Home = (props) => {
+  return <HomePage {...props} />
+}
+
+export default Home
---
+++ src/pages/Home/index.js
@@ -0,0 +1,56 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+import Head from 'next/head'
+import style from './style.module.css'
+
+const HomePage = () => {
+  const title = 'Very Usefull Tools to Remember - VUTTR'
+  const metaDescription =
+    "Very Usefull Tools to Remember, don't waste your time thinking, just write down the tool! Come See!"
+  const metaOgImage = '/static/img/home-seo.png'
+  const metaKeywords =
+    'notebook, tools, usefull, remember, save, notes, bossabox, challenge, frontend'
+
+  const scheme = {
+    '@context': 'http://schema.org',
+    '@type': 'ImageObject',
+    name: title,
+    description:
+      "Very Usefull Tools to Remember, don't waste your time thinking, just write down the tool! Come See!",
+    contentUrl: '/static/img/home-seo.png',
+    thumbnail: '/static/img/home-seo.png',
+    fileFormat: 'image/jpeg',
+    sourceOrganization: 'localhost.com',
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@danielbarion" />
+        <meta name="twitter:creator" content="@danielbarion" />
+        <meta name="description" content={metaDescription} />
+        <meta name="keywords" content={metaKeywords} />
+        <link rel="canonical" href="https://localhost/" />
+        <meta property="og:locale" content="pt_BR" />
+        <meta property="og:title" content={title} />
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content={metaDescription} />
+        <meta property="og:image" content={metaOgImage} />
+        <meta property="og:url" content="https://localhost/" />
+        <meta name="robots" content="index, follow" />
+
+        <script
+          type="application/ld+json"
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(scheme) }}
+        />
+      </Head>
+
+      <main className={style.mainContent}>lorem ipsum</main>
+    </div>
+  )
+}
+
+export default HomePage
